4baaf4e25b7868eee824e0424374762bd224ca67
mopemoepe <yutaka.matsubara@gmail.com>
2012-11-24 15:05:47 +0900

    Write column metadata

--- example/simple.py
+++ example/simple.py
@@ -3,6 +3,8 @@ from dismage import *
 def r(cmd, cmd_data, start_result):
 
     print("cmd:%s data:%s start_response:%s" % (cmd, cmd_data, start_result))
+    if "COMMIT" in cmd_data:
+        return
     #column = (type, size, name)
     columns = [(COLUMN_TYPE_VARCHAR, 10, 'name'), (COLUMN_TYPE_INT24, 8, 'age')]
     start_result(columns=columns)
--- src/result.c
+++ src/result.c
@@ -80,13 +80,11 @@ ResultObject_call(PyObject *o, PyObject *args, PyObject *kwargs)
 
 
 static io_state
-write_columns_count(client_t *client, int count)
+send_result_data_internal(client_t *client, bool buffer)
 {
     drizzle_return_t ret;
 
-    drizzle_result_set_column_count(client->result, (uint16_t)count);
-
-    ret = drizzle_result_write(client->con, client->result, false);
+    ret = drizzle_result_write(client->con, client->result, buffer);
     if (ret == DRIZZLE_RETURN_IO_WAIT) {
         return STATE_IO_WAIT;
     } else if (ret == DRIZZLE_RETURN_OK) {
@@ -101,28 +99,146 @@ write_columns_count(client_t *client, int count)
 }
 
 static io_state
-write_column_info(ClientObject *clientObj, ResultObject *result)
+send_result_data(ClientObject *clientObj, bool buffer)
+{
+    io_state state = STATE_ERROR;
+    client_t *client = clientObj->client;
+    while (1) {
+        state = send_result_data_internal(client, buffer);
+        state = check_state(clientObj, state);
+        BDEBUG("send_result_data state:%d", state);
+        if (state == STATE_OK) {
+            break;
+        } else if (state == STATE_ERROR) {
+            return state;
+        }
+    }
+    return state;
+}
+
+static io_state
+write_columns_count(ClientObject *clientObj, int count)
+{
+    client_t *client = clientObj->client;
+
+    drizzle_result_set_column_count(client->result, (uint16_t)count);
+    return send_result_data(clientObj, false);
+}
+
+static void
+set_column_table_info(drizzle_column_st *column, ResultObject *start_result)
+{
+    char *catalog = NULL;
+    char *db = NULL;
+    char *table = NULL;
+    
+    // set catalog 
+    if (start_result->catalog == NULL) {
+        drizzle_column_set_catalog(column, "dismage_catalog");
+    } else {
+        catalog = PyBytes_AS_STRING(start_result->catalog);
+        drizzle_column_set_catalog(column, catalog);
+    }
+
+    // set db
+    if (start_result->db == NULL) {
+        drizzle_column_set_db(column, "dismage_db");
+    } else {
+        db = PyBytes_AS_STRING(start_result->db);
+        drizzle_column_set_db(column, db);
+    }
+
+    //set table 
+    if (start_result->table== NULL) {
+        drizzle_column_set_table(column, "dismage_table");
+        drizzle_column_set_orig_table(column, "dismage_table");
+    } else {
+        table = PyBytes_AS_STRING(start_result->table);
+        drizzle_column_set_table(column, table);
+        drizzle_column_set_orig_table(column, table);
+    }
+
+    drizzle_column_set_charset(column, 8);
+}
+
+static io_state
+set_columns_info(ClientObject *clientObj, drizzle_column_st *column, PyObject *item)
+{
+    PyObject *type, *size, *name;
+    io_state state = STATE_ERROR;
+    drizzle_column_type_t dtype = DRIZZLE_COLUMN_TYPE_VARCHAR;
+    drizzle_return_t ret ;
+    uint32_t dsize = 0;
+    char *dname = NULL;
+    client_t *client = clientObj->client;
+
+    type = PyTuple_GET_ITEM(item, 0);
+    size = PyTuple_GET_ITEM(item, 1);
+    name = PyTuple_GET_ITEM(item, 2);
+    
+    // Check type
+    if (type == NULL || !PyLong_Check(type)) {
+        return STATE_ERROR;
+    }
+
+    // Check size
+    if (size == NULL || !PyLong_Check(size)) {
+        return STATE_ERROR;
+    }
+    
+    //
+    if (name == NULL || !PyBytes_Check(name)) {
+        return STATE_ERROR;
+    }
+    
+    dtype = (drizzle_column_type_t)PyLong_AsLong(type);
+    dsize = (uint32_t)PyLong_AsLong(size);
+    dname = PyBytes_AS_STRING(name);
+
+    drizzle_column_set_type(column, dtype);
+    drizzle_column_set_size(column, dsize);
+    drizzle_column_set_name(column, dname);
+    drizzle_column_set_orig_name(column, dname);
+
+    while (1) {
+        ret = drizzle_column_write(client->result, column);
+        if (ret == DRIZZLE_RETURN_IO_WAIT) {
+            state = STATE_IO_WAIT;
+        } else if (ret == DRIZZLE_RETURN_OK) {
+            state = STATE_OK;
+        } else {
+            //ERROR
+            RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
+            return state;
+        }
+        state = check_state(clientObj, state);
+        BDEBUG("column write state:%d", state);
+        if (state != STATE_IO_WAIT) {
+            return state;
+        }
+    }
+    return state;
+}
+
+static io_state
+write_columns_info(ClientObject *clientObj)
 {
     drizzle_column_st column;
     drizzle_return_t ret;
-    PyObject *columns, *iterator = NULL, *item = NULL;
-    PyObject *type, *size, *name;
+    PyObject *iterator = NULL, *item = NULL;
 
     io_state state = STATE_ERROR;
     client_t *client = clientObj->client;
+    ResultObject *start_result = (ResultObject*)clientObj->start_result;
     
     if (drizzle_column_create(client->result, &column) == NULL) {
         RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
         goto error;
     }
+    
+    set_column_table_info(&column, start_result);
 
-    drizzle_column_set_catalog(&column, "sqlite");
-    drizzle_column_set_db(&column, "sqlite_db");
-    drizzle_column_set_table(&column, "sqlite_table");
-    drizzle_column_set_orig_table(&column, "sqlite_table");
-    drizzle_column_set_charset(&column, 8);
-
-    iterator = PyObject_GetIter(result->columns);
+    iterator = PyObject_GetIter(start_result->columns);
     if (PyErr_Occurred()){
         goto error; 
     }
@@ -139,64 +255,22 @@ write_column_info(ClientObject *clientObj, ResultObject *result)
                          (int)PyTuple_Size(item));
             goto error;
         }
-        
-        type = PyTuple_GET_ITEM(item, 0);
-        size = PyTuple_GET_ITEM(item, 1);
-        name = PyTuple_GET_ITEM(item, 2);
-        
-
-
-        drizzle_column_set_type(&column, DRIZZLE_COLUMN_TYPE_VARCHAR);
-        drizzle_column_set_size(&column, 0);
-        drizzle_column_set_name(&column, "");
-        drizzle_column_set_orig_name(&column, "");
-
-        while (1) {
-            ret = drizzle_column_write(client->result, &column);
-            if (ret == DRIZZLE_RETURN_IO_WAIT) {
-                state = STATE_IO_WAIT;
-            } else if (ret == DRIZZLE_RETURN_OK) {
-                state = STATE_OK;
-            } else {
-                //ERROR
-                RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
-                goto error;
-            }
-            state = check_state(clientObj, state);
-            BDEBUG("column write state:%d", state);
-            if (state == STATE_OK) {
-                Py_DECREF(item);
-                break;
-            } else if (state == STATE_ERROR) {
-                goto error;
-            }
+        state = set_columns_info(clientObj, &column, item);
+        Py_DECREF(item);
+        if (state == STATE_ERROR) {
+            item = NULL;
+            goto error;
         }
+        
     }
     /* drizzle_column_free(&column); */
 
     drizzle_result_set_eof(client->result, true);
-
-    while (1) {
-        ret = drizzle_result_write(client->con, client->result, false);
-        if (ret == DRIZZLE_RETURN_IO_WAIT) {
-            state = STATE_IO_WAIT;
-        } else if (ret == DRIZZLE_RETURN_OK) {
-            state = STATE_OK;
-        } else {
-            //ERROR
-            RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
-            goto error;
-        }
-        state = check_state(clientObj, state);
-        BDEBUG("column write state:%d", state);
-        if (state == STATE_OK) {
-            Py_DECREF(item);
-            break;
-        } else if (state == STATE_ERROR) {
-            goto error;
-        }
-    }
-
+    state = send_result_data(clientObj, false);
+    Py_XDECREF(item);
+    Py_XDECREF(iterator);
+    
+    return state;
 
 error:
     Py_XDECREF(item);
@@ -206,68 +280,78 @@ error:
     return STATE_ERROR;
 }
 
-static io_state 
-return_result(client_t *client)
+static io_state
+write_rows(ClientObject *clientObj, PyObject *resObj, uint32_t count)
 {
-    drizzle_result_st *result = NULL;
-    drizzle_con_st *con = NULL;
-    drizzle_return_t ret = DRIZZLE_RETURN_OK;
+    io_state state = STATE_ERROR;
 
-    con = client->con;
-    result = client->result;
+    return state;
+}
 
-    ret = drizzle_result_write(con, result, true);
-    if (ret == DRIZZLE_RETURN_IO_WAIT) {
-        return STATE_IO_WAIT;
-    } else if (ret == DRIZZLE_RETURN_OK) {
-        drizzle_result_free(client->result);
-        client->result = NULL;
-        client->state = CALL_HANDLER;
-        return STATE_OK;
-    } else {
-        //ERROR
-        RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
-        drizzle_result_free(client->result);
-        client->result = NULL;
-        return STATE_ERROR;
+static PyObject*
+return_simple_result(ClientObject *clientObj)
+{
+    io_state state = STATE_ERROR;
+
+    state = send_result_data(clientObj, true);
+    if (state == STATE_ERROR) {
+        return NULL;
     }
+    Py_RETURN_NONE;
+}
+
+static PyObject*
+return_row_result(ClientObject *clientObj, PyObject *resObj)
+{
+    io_state state = STATE_ERROR;
+    PyObject *columns = NULL;
+    int count = 0;
+
+    ResultObject *start_result = (ResultObject*)clientObj->start_result;
+    columns = start_result->columns;
+    count = PyList_GET_SIZE(columns);
+
+    state = write_columns_count(clientObj, count);
+    DEBUG("write_columns_count clientObj:%p count:%d", clientObj, count);
+    if (state == STATE_ERROR) {
+        return NULL;
+    }
+    state = write_columns_info(clientObj);
+    DEBUG("write_columns_info clientObj:%p resObj:%p", clientObj, resObj);
+    if (state == STATE_ERROR) {
+        return NULL;
+    }
+
+    // rows
+    state = write_rows(clientObj, resObj, count);
+    if (state == STATE_ERROR) {
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
 }
 
 PyObject* 
 write_result(ClientObject *clientObj, PyObject *resObj)
 {
     drizzle_result_st *result = NULL;
-    drizzle_con_st *con = NULL;
-    drizzle_return_t ret = DRIZZLE_RETURN_OK;
-    io_state state = STATE_ERROR;
-
-    client_t *client = clientObj->client;
-    con = client->con;
     
-    result = drizzle_result_create(con, NULL);
+    client_t *client = clientObj->client;
+    result = drizzle_result_create(client->con, NULL);
     if (result == NULL) {
-        RDEBUG("ret %d:%s", ret, drizzle_error(drizzle_con_drizzle(client->con)));
+        RDEBUG("ret %s", drizzle_error(drizzle_con_drizzle(client->con)));
         return NULL;
     }
 
     client->result = result;
 
-    while (1) {
-        if (Py_None == resObj) {
-            DEBUG("return None");
-            state = return_result(client);
-        } else {
-            state = return_result(client);
-        }
-        state = check_state(clientObj, state);
-        BDEBUG("handshake_return_result state:%d", state);
-        if (state == STATE_OK) {
-            break;
-        } else if (state == STATE_ERROR) {
-            return NULL;
-        }
+    if (Py_None == resObj) {
+        DEBUG("return None");
+        return return_simple_result(clientObj);
+    } else {
+        DEBUG("return Rows");
+        return return_row_result(clientObj, resObj);
     }
-    Py_RETURN_NONE;
 }
 
 static PyMethodDef ResultObject_methods[] = {
