06f45b9ec8b794bee8f4791d2aaf690a9ce4a3d6
mopemoepe <yutaka.matsubara@gmail.com>
2012-11-26 19:55:21 +0900

    Update example

--- example/simple.py
+++ example/simple.py
@@ -1,13 +1,11 @@
 from dismage import *
-from multiprocessing import Process
-import signal
 
 workers = []
 
 def app(cmd, cmd_data, start_result):
 
     print("cmd:%s data:%s start_response:%s" % (cmd, cmd_data, start_result))
-    if "COMMIT" in cmd_data:
+    if cmd == COMMAND_INIT_DB or "COMMIT" in cmd_data:
         return
     #column = (type, size, name)
     columns = [(COLUMN_TYPE_VARCHAR, 10, 'name'), (COLUMN_TYPE_INT24, 8, 'age')]
