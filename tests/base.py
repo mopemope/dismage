681105b40d73f89c5b78064fd5efa6b99e2e615d
mopemoepe <yutaka.matsubara@gmail.com>
2012-12-11 10:19:41 +0900

    Add test

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,5 @@
-import dimage
-from dimage import patch
+import dismage
+from dismage import patch
 patch.patch_all()
 import time
 import pymysql
@@ -18,7 +18,6 @@ def start_server(app):
 
 class ServerRunner(object):
 
-
     def __init__(self, app):
         self.app = app
         self.running = False
@@ -27,11 +26,11 @@ class ServerRunner(object):
         if self.running:
             return
 
-        dismage.listen()
+        dismage.listen(port=3307)
         self.running = True
         if shutdown:
             dismage.schedule_call(1, dismage.shutdown, 1)
-        dimage.run(app)
+        dismage.run(self.app)
 
 class ClientRunner(object):
 
@@ -40,21 +39,22 @@ class ClientRunner(object):
         self.func = func
         self.app = app
         self.shutdown = shutdown
+        self.result = None
 
     def run(self):
 
         def _call():
             try:
                 r = self.func()
-                self.receive_data = r
+                self.result = r
             finally:
                 if self.shutdown:
                     dismage.shutdown(1)
 
-        dimage.spawn(_call)
+        dismage.spawn(_call)
 
     def get_result(self):
-        return
+        return (self.app.cmd_type, self.app.cmd, self.result)
 
 def run_client(client_func=None, app_factory=None):
     application = app_factory()
@@ -62,5 +62,33 @@ def run_client(client_func=None, app_factory=None):
     r = ClientRunner(application, client_func)
     r.run()
     s.run()
-    return
+    return r.get_result()
+
+def run_query(sql):
+    import pymysql
+    con = pymysql.connect(host="127.0.0.1", user="admin", passwd="", port=3307, db="test")
+    try:
+        c = con.cursor()
+        c.execute(sql)
+        r = c.fetchone()
+        return r
+    finally:
+        con.close()
+
+class BaseApp(object):
+    
+    cmd_type = None
+    cmd = None
+
+    def __call__(self, cmd_type, cmd, start_result):
+
+        self.cmd_type = cmd_type
+        self.cmd = cmd
+
+        if cmd_type == dismage.COMMAND_INIT_DB or "COMMIT" in cmd:
+            return
+        columns = [(dismage.COLUMN_TYPE_VARCHAR, 10, 'name'), (dismage.COLUMN_TYPE_INT24, 8, 'age')]
+        start_result(columns=columns)
+        return [('john', 30), ('smith', 28)]
+
 
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,2 +1,20 @@
-from dismage import *
+from base import *
+import dismage
+
+class App(BaseApp):
+    pass
+    
+
+def test_simple():
+    
+    query = "SELECT * FROM test"
+
+    def client():
+
+        return run_query(query)
+    
+    cmd_type, cmd, q  = run_client(client, App)
+    assert(cmd_type == dismage.COMMAND_DRIZZLE_QUERY)
+    assert(cmd == query)
+
 
